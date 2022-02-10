0a31b889cc55463db6e0887dd0c2e08e928c3e45
SigvardsK <59480272+SigvardsK@users.noreply.github.com>
2022-02-10 21:21:03 +0200

    commit
    index.html - added cell-index to each cell + added restart button
    stylesheet - changed game board background
    app.js - created main function for PvP game

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // define variables
 let userScore = 0;
 let computerScore = 0;
@@ -8,11 +9,102 @@ const result_p = document.querySelector(".result > p");
 const x_div = document.getElementById("x");
 const o_div = document.getElementById("o");
 
+// state when game happens -- so that know when to end
+let gameActive = true;
+// can be user or computer
+let currentPlayer = "X";
+// key moment --> each game board with each cell as a variable
+let gameState = ["", "", "", "", "", "", "", "", ""]; 
 
-// click function
-x_div.addEventListener('click', function() {
-        console.log("x");
-})
-o_div.addEventListener('click', function () {
-    console.log("o");
-})
+const statusDisplay = document.querySelector('.game--status');
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+// winning conditions - when 3 cells have the same value. 
+// Numbers - from html file - id of each cell
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+// function that checks if a cell is played and if not fils it
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+// player change function
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+// main game function - checking result and changing players
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+// click function in cell
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+// restart game function
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+// click functions on cells and restart button
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
